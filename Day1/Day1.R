bfaa83eb8da18ae82298292c69f7f554404baf92
Lauren Schmiedeler <leschmiedeler@gmail.com>
2022-12-01 11:23:50 -0600

    completed day 1 puzzles

---
+++ Day1/Day1.R
@@ -0,0 +1,17 @@
+connection <- file(description = "Day1Input.txt", open = "r")
+all_calories <- c()
+calories <- 0
+for (line in readLines(connection)) {
+  if (line == "") {
+    all_calories <- append(all_calories, calories)
+    calories <- 0
+  }
+  else {
+    calories <- calories + as.integer(line)
+  }
+}
+close(connection)
+
+# puzzle answers
+print(paste0("puzzle 1 answer = ", max(all_calories)))
+print(paste0("puzzle 2 answer = ", sum(sort(all_calories, decreasing = TRUE)[1:3])))
