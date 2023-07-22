f017c4dcbfd46f6d345822ef6c9b83fe52f80d0f
Lauren Schmiedeler <leschmiedeler@gmail.com>
2023-07-22 09:41:53 -0500

    cleaned up day 15

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -20,18 +20,20 @@ def find_n_no_beacon(union_no_beacon_polys, beacons, y):
     horizontal_line = LineString([(union_no_beacon_polys.bounds[0], y), (union_no_beacon_polys.bounds[2], y)])
     return (int(intersection(horizontal_line, union_no_beacon_polys).length) + 1 - len([p for p in list(set(beacons)) if p.y == y]))
 
+def find_tuning_freq(distress_beacon):
+    exterior_points = distress_beacon.exterior.xy
+    return (int(((min(exterior_points[0]) + max(exterior_points[0])) / 2) * 4000000 + ((min(exterior_points[1]) + max(exterior_points[1])) / 2)))
+
 def find_distress_beacon_tuning_freq(union_no_beacon_polys, min_bound, max_bound):
     boundary = Polygon([(min_bound, min_bound), (min_bound, max_bound), (max_bound, max_bound), (max_bound, min_bound)])
     distress_beacon = boundary.difference(union_no_beacon_polys)
     if type(distress_beacon) == Polygon:
-        exterior_points = distress_beacon.exterior.xy
-        return (int(((min(exterior_points[0]) + max(exterior_points[0])) / 2) * 4000000 + ((min(exterior_points[1]) + max(exterior_points[1])) / 2)))
+        return find_tuning_freq(distress_beacon)
     else:
         possible_beacon = list(distress_beacon.geoms)
         for p in possible_beacon:
             if p.area == 2:
-                exterior_points = p.exterior.xy
-                return (int(((min(exterior_points[0]) + max(exterior_points[0])) / 2) * 4000000 + ((min(exterior_points[1]) + max(exterior_points[1])) / 2)))
+                return find_tuning_freq(p)
 
 # puzzle answers
 print("puzzle 1 answer =", find_n_no_beacon(union_no_beacon_polys, beacons, 2000000))
